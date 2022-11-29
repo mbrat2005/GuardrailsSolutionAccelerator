c7c5bdd8056e84ff383db070657c1bfe34968b20
Matthew Bratschun <matthew.bratschun@microsoft.com>
2022-11-29 10:44:46 -0700

    Revert "target mgmt group id"
    This reverts commit 1ba832e3a6d506efbc897562cfdede9c08e6af66.

--- src/GuardrailsSolutionAcceleratorSetup/modules/Confirm-GSAConfigurationParameters/Confirm-GSAConfigurationParameters.psm1
+++ src/GuardrailsSolutionAcceleratorSetup/modules/Confirm-GSAConfigurationParameters/Confirm-GSAConfigurationParameters.psm1
@@ -246,7 +246,6 @@ Function Confirm-GSAConfigurationParameters {
     $config['runtime']['tenantId'] = $tenantId
     $config['runtime']['tenantDomainUPN'] = $tenantDomainUPN
     $config['runtime']['tenantRootManagementGroupId'] = '/providers/Microsoft.Management/managementGroups/{0}' -f $tenantId
-    $config['runtime']['lighthouseTargetManagementGroupID'] = '/providers/Microsoft.Management/managementGroups/{0}' -f $lighthouseTargetManagementGroupID
     $config['runtime']['userId'] = $userId
     $config['runtime']['tagsTable'] = $tagsTable
     $config['runtime']['deployLAW'] = $true
