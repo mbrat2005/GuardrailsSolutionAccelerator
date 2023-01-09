8c176c8f973b3c31d2dab4365e43939c40c27d0b
Matthew Bratschun <matthew.bratschun@microsoft.com>
2023-01-09 11:21:54 -0700

    more docs

--- src/GuardrailsSolutionAcceleratorSetup/modules/Get-GSAExportedConfig/Get-GSAExportedConfig.psm1
+++ src/GuardrailsSolutionAcceleratorSetup/modules/Get-GSAExportedConfig/Get-GSAExportedConfig.psm1
@@ -40,7 +40,7 @@ Function Get-GSAExportedConfig {
 
     If (!$yes.IsPresent) {
         Write-Host "Retrieving the latest configuration from Key Vault $KeyVaultName. To find a previous version, use the Azure Portal and browse the secret versions for secret name 'gsaConfigExportLatest'. Note that each secret version has tags with deployment details."
-        Write-Warning "Executing this command will output your config value to the console, which may be a security concern. It is recommended to run this command with the syntax similar to below, where the output from Get-GSAExportedConfig is passed through the pipline. See Get-Help Get-GSAExportedConfig for more info:`n`n`tGet-GSAExportedConfig -KeyVaultName guardrails-12345 | Deploy-GuardrailsSolutionAccelerator -update`n`nPress ENTER to proceed or CTRL+C to cancel."
+        Write-Warning "Executing this command will output your config value to the console, which may be a security concern. It is recommended to run this command with the syntax similar to below, where the output from Get-GSAExportedConfig is passed through the pipline. See Get-Help Get-GSAExportedConfig for more info. To surpress this warning, include the '-yes' parameter.:`n`n`tGet-GSAExportedConfig -KeyVaultName guardrails-12345 -yes | Deploy-GuardrailsSolutionAccelerator -update`n`nPress ENTER to proceed or CTRL+C to cancel."
         $null = Read-Host
     }
     
