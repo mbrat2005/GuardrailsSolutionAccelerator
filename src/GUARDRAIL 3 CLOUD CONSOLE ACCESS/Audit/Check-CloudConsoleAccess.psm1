1daf1fedc7fc9f1af1aa45d17d30c187220bcbd2
Matthew Bratschun <matthew.bratschun@microsoft.com>
2023-01-27 12:36:11 -0700

    fix CAP check syntax

--- src/GUARDRAIL 3 CLOUD CONSOLE ACCESS/Audit/Check-CloudConsoleAccess.psm1
+++ src/GUARDRAIL 3 CLOUD CONSOLE ACCESS/Audit/Check-CloudConsoleAccess.psm1
@@ -26,6 +26,7 @@ function Get-CloudConsoleAccess {
         Write-Warning "Error: Failed to call Microsoft Graph REST API at URL '$locationsBaseAPIUrl'; returned error message: $_"
     }
     # $locations
+    
     $validLocations = @()
     foreach ($location in $locations) {
         #Determine location conditions
@@ -45,7 +46,7 @@ function Get-CloudConsoleAccess {
             $response = Invoke-GraphQuery -urlPath $CABaseAPIUrl -ErrorAction Stop
 
             $caps = $response.Content.value
-            $validPolicies = $caps | Where-Object { $_.conditions.locations.includeLocations -in $validLocations.ID -and $cap.state -eq 'enabled' }
+            $validPolicies = $caps | Where-Object { $_.conditions.locations.includeLocations -in $validLocations.ID -and $_.state -eq 'enabled' }
         }
         catch {
             $Errorlist.Add("Failed to call Microsoft Graph REST API at URL '$CABaseAPIUrl'; returned error message: $_")
